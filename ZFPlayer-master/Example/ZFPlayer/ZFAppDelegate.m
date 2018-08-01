bd47abaeac2eab587fd270318ba5abbe55bc4680
chenjinzhi <15527747037@163.com>
2018-08-01 17:48:26 +0800

    测试分支3
    测试分支3

--- ZFPlayer-master/Example/ZFPlayer/ZFAppDelegate.m
+++ ZFPlayer-master/Example/ZFPlayer/ZFAppDelegate.m
@@ -14,7 +14,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 //    [ZFPlayerLogManager setLogEnable:YES];
     NSLog(@"测试--------3");
-    NSLog(@"分支测试--------1");
+    NSLog(@"分支测试--------3");
     return YES;
 }
 
