dfeb498965a12791c73e73914582c97b189e92d9
chenjinzhi <15527747037@163.com>
2018-08-01 17:54:24 +0800

    合并分支2到分支3
    合并分支2到分支3

--- ZFPlayer-master/Example/ZFPlayer/ZFAppDelegate.m
+++ ZFPlayer-master/Example/ZFPlayer/ZFAppDelegate.m
@@ -14,12 +14,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 //    [ZFPlayerLogManager setLogEnable:YES];
     NSLog(@"测试--------3");
-<<<<<<< HEAD
     NSLog(@"分支测试--------3");
-=======
     NSLog(@"分支测试--------1");
     NSLog(@"分支测试--------2");
->>>>>>> 分支2
+
     return YES;
 }
 
